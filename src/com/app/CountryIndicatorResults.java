006cdde01518f7268044e5b63107be0550b6e20c
kwketh <keth@kwketh.com>
2013-11-05 14:40:44 +0000

    added todo paragraph in CountryIndicatorResults.java to support plotting multiple values

--- src/com/app/CountryIndicatorResults.java
+++ src/com/app/CountryIndicatorResults.java
@@ -22,6 +22,7 @@ public class CountryIndicatorResults extends Observable
     String m_countryCode;
     Indicator m_indicator;
     
+    /* todo: allow array of countryCodes for plotting values */
     public CountryIndicatorResults(String countryCode, Indicator indicator) 
     {
         /* Default values */
