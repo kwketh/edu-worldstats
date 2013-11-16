a9751663472a835d8dd448836cdba7d106559325
Charlotte Murray <charlottelouisemurray@gmail.com>
2013-11-16 21:49:24 +0000

    social development layout added and all layouts updated

--- src/com/app/SocialDevelopmentFragment.java
+++ src/com/app/SocialDevelopmentFragment.java
@@ -7,16 +7,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class SocialDevelopmentFragment extends Fragment {
-    
+public class SocialDevelopmentFragment extends Fragment
+{
+
+    SeekBar yearSeek;
+    TextView year;
+    TextView maleLifeExpectancy;
+    TextView femaleLifeExpectancy;
+    TextView femaleParliamentSeats;
+    TextView fertilityRate;
+    TextView cpiaRating;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.social_development_fragment, container, false);
-         
+
+        View rootView = inflater.inflate(R.layout.social_development_fragment,
+                container, false);
+
+        maleLifeExpectancy = (TextView) rootView
+                .findViewById(R.id.maleLifeExpectancy);
+        femaleLifeExpectancy = (TextView) rootView
+                .findViewById(R.id.femaleLifeExpectancy);
+        femaleParliamentSeats = (TextView) rootView
+                .findViewById(R.id.proportionOfSeats);
+        fertilityRate = (TextView) rootView.findViewById(R.id.fertilityRate);
+        cpiaRating = (TextView) rootView.findViewById(R.id.cpiaRating);
+
+        year = (TextView) rootView.findViewById(R.id.year);
+        yearSeek = (SeekBar) rootView.findViewById(R.id.seekBar);
+        updateValues(yearSeek.getProgress());
+
+        yearSeek.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+                updateValues(arg1);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+
+            }
+        });
+
         return rootView;
     }
 
+    private void updateValues(int value) {
+        year.setText((value + 1960) + "");
+        // Update all fields here
+    }
 }
