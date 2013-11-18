a7ea16ca3f1504a30db8508723b2cf8f9f0129e1
kwketh <keth@kwketh.com>
2013-11-18 07:37:44 +0000

    DataPoint now supports parsing number of decimals value from the json and implements formatted value

--- src/com/app/worldbankapi/DataPoint.java
+++ src/com/app/worldbankapi/DataPoint.java
@@ -7,8 +7,12 @@ import org.json.JSONObject;
 
 public class DataPoint
 {    
-    final static DecimalFormat prettyFormat = new DecimalFormat("#.##");
-    Double m_value;
+    static DecimalFormat prettyDateFormat = new DecimalFormat("#.##");
+    static DecimalFormat prettyValueFormat;
+        
+    Double m_value = 0.0;
+    int m_decimals = 0;
+    boolean m_null = false;
     
     DataPoint()
     {
@@ -24,14 +28,32 @@ public class DataPoint
     {
         return m_value;
     }
-
+    
+    public String getFormattedValue()
+    {
+        String format = "#,###";
+        if (m_decimals > 0) format += ".";
+        for (int i = 0; i < m_decimals; i++) format += "#";
+        prettyValueFormat = new DecimalFormat(format);        
+        return prettyValueFormat.format(getValue());
+    }
+    
+    public boolean isNullValue()
+    {
+        return m_null;
+    }
+        
     public String toString()
     {
-        return "[DataPoint value=" + prettyFormat.format(m_value) + "]";
+        return "[DataPoint value=" + prettyDateFormat.format(m_value) + "]";
     }
     
     public void fromJSON(JSONObject json) throws JSONException
     {
-        m_value = json.getDouble("value");
+        m_null = json.isNull("value");
+        if (!m_null) {
+            m_value = json.getDouble("value");
+            m_decimals = json.optInt("decimal");
+        }
     }    
 }
