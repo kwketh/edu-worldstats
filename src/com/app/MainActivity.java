6386b3f456ba667616a0fc3fe8b7714f5541b6bf
kwketh <keth@kwketh.com>
2013-11-19 07:25:23 +0000

    hopefully this can improve the application colors, according to the android docs

--- src/com/app/MainActivity.java
+++ src/com/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -17,6 +18,8 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_main);
+        
+        getWindow().setFormat(PixelFormat.RGBA_8888); 
         
         btnChooseCountry = (Button) findViewById(R.id.btn_choose_a_country);
         btnCompareCountries = (Button) findViewById(R.id.btn_compare_countries);           
