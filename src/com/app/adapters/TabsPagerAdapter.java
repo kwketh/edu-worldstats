0acee2bead14619f077deb5731757664af3bb21a
kwketh <keth@kwketh.com>
2013-11-27 12:46:23 +0000

    TabsPagerAdapter will no longer constantly create new fragments but rather reuse them

--- src/com/app/adapters/TabsPagerAdapter.java
+++ src/com/app/adapters/TabsPagerAdapter.java
@@ -9,32 +9,55 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class TabsPagerAdapter extends FragmentPagerAdapter {
-    
-    public TabsPagerAdapter(FragmentManager fm) {
-        super(fm);
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter
+{
+    private HashMap<Integer, Fragment> m_fragmentsMap;
+
+    public TabsPagerAdapter(FragmentManager fragmentManager)
+    {
+        super(fragmentManager);
+        m_fragmentsMap = new HashMap<Integer, Fragment>();
     }
  
     @Override
-    public Fragment getItem(int index) {
- 
-        switch (index) {
-        case 0:
-            return new BasicInfoFragment();
-        case 1:
-            return new EducationFragment();
-        case 2:
-            return new IndustryFragment();
-        case 3:
-            return new SocialDevelopmentFragment();
+    public Fragment getItem(int index)
+    {
+        if (!m_fragmentsMap.containsKey(index))
+        {
+            Fragment fragment;
+            switch (index)
+            {
+                case 0:
+                    fragment = new BasicInfoFragment();
+                    break;
+
+                case 1:
+                    fragment = new EducationFragment();
+                    break;
+
+                case 2:
+                    fragment = new IndustryFragment();
+                    break;
+
+                case 3:
+                    fragment = new SocialDevelopmentFragment();
+                    break;
+
+                default:
+                    throw new Error("No fragment found for index " + index);
+            }
+            m_fragmentsMap.put(index, fragment);
         }
- 
-        return null;
+
+        return m_fragmentsMap.get(index);
     }
  
     @Override
-    public int getCount() {
-        // get item count - equal to number of tabs
+    public int getCount()
+    {
         return 4;
     }
     
