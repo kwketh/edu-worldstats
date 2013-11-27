5e2ca63b1a2eb2120c4358a9d684af320b635c81
Charlotte Murray <charlottelouisemurray@gmail.com>
2013-11-27 14:46:09 +0000

    minor changes

---
+++ src/com/app/activities/RSSActivity.java
@@ -0,0 +1,28 @@
+package com.app.activities;
+
+import com.app.R;
+import com.app.R.layout;
+import com.app.R.menu;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class RSSActivity extends ListActivity {
+    
+    List headlines;
+    List links;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+ 
+        
+        
+        
+        
+    }
+}
